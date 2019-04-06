Project-Xtended/packages_apps_Settings

Integrate OmniJaws Settings for WeatherTile Long Click

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -141,6 +141,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.msm.xtended.Xtensions;
 import com.xtended.settings.fragments.about.Update;
+import com.msm.xtended.fragments.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -270,6 +271,7 @@ public class SettingsGateway {
 	    ScreenStabilization.class.getName(),
             RunningServices.class.getName(),
 	    Update.class.getName(),
+	    OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

lavas61/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -145,6 +145,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.colt.settings.ColtSettings;
 import com.colt.settings.ColtSettingsLayout;
+import com.colt.settings.fragments.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -278,6 +279,7 @@ public class SettingsGateway {
             FlingSettingsActivity.class.getName(),
             PulseSettingsActivity.class.getName(),
             RunningServices.class.getName()
+	    OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {